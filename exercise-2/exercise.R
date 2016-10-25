b5c1416ff12dc4ba938f40026ef74abe4cb667b1
Josh Keating <josh.keating7@gmail.com>
2016-10-25 15:00:52 -0700

    another day another class

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -4,11 +4,20 @@
 # This is a perfect situation in which writing a function will allow you to better structure your
 # code, and give a name to a repeated task.
 library(jsonlite)
-setwd('~/Documents/info-201/m10-apis/exercise-2/')
+setwd("/home/josh/School_16-17/Info-201/bin/m10-apis/exercise-2/")
+
+# Base URL of API
+base <- 'https://api.spotify.com/v1/'
+# Parameters 
+artist <- 'Isaiah Rashad' 
+artist.query <-  paste0(base, 'search?', 'q=', artist, '&type=artist')
+
+data <- fromJSON(query.url)
+
 
 
 # Write a function that allows you to specify an artist, and returns the top 10 tracks of that artist
-
+#top.10 <- function(artist)
 
 
 # What are the top 10 tracks by Nelly?
